2e2ad1644a7d87f8ea114e08531aef9258d6e7a5
Julia Starostenko <julia.starostenko@gmail.com>
2021-12-10 11:53:43 +0200

    Changed app.js and adding today's data

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -8,12 +8,13 @@ $('#loadTop').click(() => {
 function loadTopRestaurant() {
 
     fetch("http://localhost:8080/restaurants/top").
-    then(response => response.json()).
-    then(json => json.forEach(restaurant => {
+    then(response => response.json())
+        .then(info => {
 
-        let line = '<h5 class="mt-2 mb-0">' + "TOP RESTAURANT WITH MOST LIKES:"  + '</h5>'
-        let line2 = '<span class="fw-bolder">' + restaurant.name + '</span>'
+            let line = '<h6 class="mt-2 mb-0">' + "TOP RESTAURANT WITH MOST LIKES:"  + '</h6>'
+            let line2 = '<span class="fw-bolder">' + info.name + '</span>'
 
-       topRestaurant.innerHTML = line + line
-    }))
-}
+            topRestaurant.innerHTML = line + line2
+
+        })}
+
